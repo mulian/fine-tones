d88fec3c77705805e339b3441e38d74d1f3b5657
mulian <Max.Hoffmann@tu-bs.de>
2018-01-14 18:10:10 +0100

    add basement gulp, browserify, babel, gulp-watch

---
+++ gulpfile.js
@@ -0,0 +1,49 @@
+const gulp = require('gulp');
+const gutil = require('gulp-util');
+const babel = require('gulp-babel');
+const watch = require('gulp-watch');
+const browserify = require('gulp-browserify');
+
+// Basic usage
+gulp.task('browserify', function() {
+    // Single entry point to browserify
+    gulp.src('js/index.js')
+        .pipe(browserify({
+          insertGlobals : true,
+          debug : !gulp.env.production
+        }))
+        .pipe(gulp.dest('./dest/'));
+});
+
+gulp.task('build_js', function() {
+  let src = './es6/*.js';
+  gulp.src(src)
+    .pipe(watch(src,function() {
+      gulp.start('browserify')
+    }))
+    // .pipe(coffeescript({bare: true}).on('error', gutil.log))
+    .pipe(babel({presets: ['env']}))
+    .pipe(gulp.dest('./js/'));
+});
+
+gulp.task('default', ['build_js']);
+
+
+// gulp.task('default', () =>
+//     gulp.src('src/app.js')
+//         .pipe(babel({
+//             presets: ['env']
+//         }))
+//         .pipe(gulp.dest('dist'))
+// );
+
+
+// gulp.task('scripts', function() {
+//     // Single entry point to browserify
+//     gulp.src('src/js/app.js')
+//         .pipe(browserify({
+//           insertGlobals : true,
+//           debug : !gulp.env.production
+//         }))
+//         .pipe(gulp.dest('./build/js'))
+// });
